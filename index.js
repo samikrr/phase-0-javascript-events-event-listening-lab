86b18dacb8ca744fc706e5dda16f43e6bdb2ebb0
Samuel Kirori <samuelkirori300@gmail.com>
2024-04-03 10:38:43 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+//create a function addingEventlistener and add addEventlistener inside then call the outer function
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+    input.addEventListener('click',function(){
+      alert ('I was clicked!');
+    });
+  }
+  addingEventListener();
+
